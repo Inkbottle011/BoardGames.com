798a5c9b76042136b291e4f87f449abee1c01a16
maxhnatyshyn-ui <maxhnatyshyn@gmail.com>
2026-03-04 03:26:16 -0500

    Add files via upload
    Skeleton JS code that can be reworked later into the main branch once a database is set up and running

---
+++ Doomlings.js
@@ -0,0 +1,44 @@
+let Age = []
+
+let catastraphy = []
+
+function gamestart() {
+    //Set amount of players
+
+    //Random select starting player
+
+    //Start New Age 
+
+    //Player turn --> run Play function --> End turn 
+
+    //Run till 3 Catastraphy Ages are Played turns are complete
+}
+
+
+function play() {
+    //Player Chooses card from hand 
+
+    //take playerhand.cards take card choosen --> place in playerhand.traitpool 
+
+    //run card effect from database(?) 
+
+    //End player turn 
+
+}
+
+function StealHandCard() {
+
+    //Take choose playerhand that need to be taken (either randomly or by player choice) 
+    //Choose randomly card to be moved from playerhand.cards --> other playerhand.cards
+}
+
+function StealTraitCard() {
+    //Take choose playerhand that need to be taken (either randomly or by player choice)
+    //Choose randomly card to be moved from playerhand.traitpool --> other playerhand.traitpool
+
+}
+
+
+
+
+
