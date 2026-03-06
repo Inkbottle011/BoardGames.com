71490d4515355f77c31450107128760d86b75283
Gaurab <tur61383@temple.edu>
2026-03-06 11:32:22 +0900

    basic table setup

---
+++ database/migrations/2026_03_06_022330_doomlings_deck.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $table->id();
+        $table->string('card_name');
+        $table->integer('points');
+        $table->string('img');
+        $table->string('text');
+        $table->string('color');
+        $table->boolean('dominant');
+        $table->boolean('action');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doomlings_deck');
+    }
+};
