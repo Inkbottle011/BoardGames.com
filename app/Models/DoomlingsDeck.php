2510370271326cfe795823548b43e86bc2254dbd
Gaurab <tur61383@temple.edu>
2026-03-07 00:47:40 +0900

    Added json file with all base game cards for doomlings which can be used with Seeder file to load into database, not yet added ages

---
+++ app/Models/DoomlingsDeck.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DoomlingsDeck extends Model
+{
+    protected $table = 'doomlings_deck';
+
+    protected $fillable = [
+        'id',
+        'card_name',
+        'img',
+        'text',
+        'color',
+        'dominant',
+        'action'
+    ];
+}
