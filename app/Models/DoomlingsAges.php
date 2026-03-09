7b562d0a38978166b5d07613ebe51b1626f9d8d0
Gaurab <tur61383@temple.edu>
2026-03-09 13:26:55 +0900

    Added all ages and fixed image links

---
+++ app/Models/DoomlingsAges.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DoomlingsAges extends Model
+{
+    protected $table = 'doomlings_ages';
+
+    protected $fillable = [
+        'id',
+        'card_name',
+        'img',
+        'catastrophe'
+    ];
+}
