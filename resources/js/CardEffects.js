4b41fe2af55f7fd1b87b7bcced63be495cee4afd
Gaurab <tur61383@temple.edu>
2026-03-13 12:14:37 +0900

    Added alot of Logic for doomlings and deck, also added empty cardeffects.js needs to be populated with what each card does to be balled in doomlings.js

---
+++ resources/js/CardEffects.js
@@ -0,0 +1,245 @@
+import { resolveCard, GameState } from "./Doomlings.js";
+import * as Deck from "./Deck.js";
+//completed
+
+//not completed
+function Echolocation_Effect(currentPlayer, players) {}
+function Imunity_Effect(currentPlayer, players) {}
+function Tiny_Effect(currentPlayer, players) {}
+function Automimicry_Effect(currentPlayer, players) {}
+function Blubber_Effect(currentPlayer, players) {}
+function Chromatophores_Effect(currentPlayer, players) {}
+function ColdBlood_Effect(currentPlayer, players) {}
+function CostlySignaling_Effect(currentPlayer, players) {}
+function EggClusters_Effect(currentPlayer, players) {}
+function Flight_Effect(currentPlayer, players) {}
+function Gills_Effect(currentPlayer, players) {}
+function IridescentScales_Effect(currentPlayer, players) {}
+function Migratory_Effect(currentPlayer, players) {}
+function PaintedShell_Effect(currentPlayer, players) {}
+function RegenerativeTissue_Effect(currentPlayer, players) {}
+function Saliva_Effect(currentPlayer, players) {}
+function Scutes_Effect(currentPlayer, players) {}
+function SelectiveMemory_Effect(currentPlayer, players) {}
+function Spiny_Effect(currentPlayer, players) {}
+function Sweat_Effect(currentPlayer, players) {}
+function Tentacles_Effect(currentPlayer, players) {}
+function Heroic_Effect(currentPlayer, players) {}
+function PackBehavior_Effect(currentPlayer, players) {}
+function Symbiosis_Effect(currentPlayer, players) {}
+function Apealing_Effect(currentPlayer, players) {}
+function Bark_Effect(currentPlayer, players) {}
+function Branches_Effect(currentPlayer, players) {}
+function DeepRoots_Effect(currentPlayer, players) {}
+function Fecundity_Effect(currentPlayer, players) {}
+function Fortunate_Effect(currentPlayer, players) {}
+function Leaves_Effect(currentPlayer, players) {}
+function Overgrowth_Effect(currentPlayer, players) {}
+function Photosynthesis_Effect(currentPlayer, players) {}
+function Pollination_Effect(currentPlayer, players) {}
+function Propagation_Effect(currentPlayer, players) {}
+function RandomFertilization_Effect(currentPlayer, players) {}
+function SelfReplicating_Effect(currentPlayer, players) {}
+function SwarmFur_Effect(currentPlayer, players) {}
+function SwarmHorns_Effect(currentPlayer, players) {}
+function SwarmMindless_Effect(currentPlayer, players) {}
+function SwarmSpots_Effect(currentPlayer, players) {}
+function SwarmStripes_Effect(currentPlayer, players) {}
+function TinyLittleMelons_Effect(currentPlayer, players) {}
+function Trunk_Effect(currentPlayer, players) {}
+function WoodyStems_Effect(currentPlayer, players) {}
+function Denial_Effect(currentPlayer, players) {}
+function Faith_Effect(currentPlayer, players) {
+    currentPlayer.worldsEndEffects.push(() => {
+        // change all traits of 1 color to another color
+    });
+}
+function OptimisticNihilism_Effect(currentPlayer, players) {}
+function Altruistic_Effect(currentPlayer, players) {}
+function Boredom_Effect(currentPlayer, players) {}
+function Confusion_Effect(currentPlayer, players) {}
+function Delicious_Effect(currentPlayer, players) {}
+function Doting_Effect(currentPlayer, players) {}
+function Eloquence_Effect(currentPlayer, players) {}
+function Fear_Effect(currentPlayer, players) {}
+function Flatulence_Effect(currentPlayer, players) {}
+function Gratitude_Effect(currentPlayer, players) {}
+function Introspective_Effect(currentPlayer, players) {}
+function Just_Effect(currentPlayer, players) {}
+function Late_Effect(currentPlayer, players) {}
+function Mindful_Effect(currentPlayer, players) {}
+function Mitochondrion_Effect(currentPlayer, players) {}
+function Morality_Effect(currentPlayer, players) {}
+function Prepper_Effect(currentPlayer, players) {}
+function Saudade_Effect(currentPlayer, players) {}
+function SelfAwareness_Effect(currentPlayer, players) {}
+function TheThirdEye_Effect(currentPlayer, players) {}
+function Camouflage_Effect(currentPlayer, players) {}
+function Vampirism_Effect(currentPlayer, players) {}
+function Viral_Effect(currentPlayer, players) {}
+function Adorable_Effect(currentPlayer, players) {}
+function BigEars_Effect(currentPlayer, players) {}
+function Clever_Effect(currentPlayer, players) {}
+function DirectlyRegister_Effect(currentPlayer, players) {}
+function Dreamer_Effect(currentPlayer, players) {}
+function FineMotorSkills_Effect(currentPlayer, players) {}
+function Impatience_Effect(currentPlayer, players) {}
+function Inventive_Effect(currentPlayer, players) {}
+function Memory_Effect(currentPlayer, players) {}
+function Nocturnal_Effect(currentPlayer, players) {}
+function Nosy_Effect(currentPlayer, players) {}
+function Parasitic_Effect(currentPlayer, players) {}
+function Persuasive_Effect(currentPlayer, players) {}
+function Poisonous_Effect(currentPlayer, players) {}
+function Selfish_Effect(currentPlayer, players) {}
+function Sneaky_Effect(currentPlayer, players) {}
+function StickySecretions_Effect(currentPlayer, players) {}
+function SuperSpreader_Effect(currentPlayer, players) {}
+function Teeth_Effect(currentPlayer, players) {}
+function Telekinetic_Effect(currentPlayer, players) {}
+function Venomous_Effect(currentPlayer, players) {}
+function ApexPredator_Effect(currentPlayer, players) {}
+function HyperIntelligence_Effect(currentPlayer, players) {}
+function Sentience_Effect(currentPlayer, players) {}
+function Antlers_Effect(currentPlayer, players) {}
+function Bad_Effect(currentPlayer, players) {}
+function Brave_Effect(currentPlayer, players) {}
+function BruteStrength_Effect(currentPlayer, players) {}
+function Endurance_Effect(currentPlayer, players) {}
+function Fangs_Effect(currentPlayer, players) {}
+function FireSkin_Effect(currentPlayer, players) {}
+function HeatVision_Effect(currentPlayer, players) {}
+function HotTemper_Effect(currentPlayer, players) {}
+function KidneyBeerHelm_Effect(currentPlayer, players) {}
+function KidneyChefsToque_Effect(currentPlayer, players) {}
+function KidneyCombover_Effect(currentPlayer, players) {}
+function KidneyElfHat_Effect(currentPlayer, players) {}
+function KidneyPartyHat_Effect(currentPlayer, players) {}
+function KidneyTiara_Effect(currentPlayer, players) {}
+function Quick_Effect(currentPlayer, players) {}
+function Reckless_Effect(currentPlayer, players) {}
+function RetractableClaws_Effect(currentPlayer, players) {}
+function StoneSkin_Effect(currentPlayer, players) {}
+function Territorial_Effect(currentPlayer, players) {}
+function Voracious_Effect(currentPlayer, players) {}
+function WarmBlood_Effect(currentPlayer, players) {}
+
+//export function
+export {
+    Echolocation_Effect,
+    Imunity_Effect,
+    Tiny_Effect,
+    Automimicry_Effect,
+    Blubber_Effect,
+    Chromatophores_Effect,
+    ColdBlood_Effect,
+    CostlySignaling_Effect,
+    EggClusters_Effect,
+    Flight_Effect,
+    Gills_Effect,
+    IridescentScales_Effect,
+    Migratory_Effect,
+    PaintedShell_Effect,
+    RegenerativeTissue_Effect,
+    Saliva_Effect,
+    Scutes_Effect,
+    SelectiveMemory_Effect,
+    Spiny_Effect,
+    Sweat_Effect,
+    Tentacles_Effect,
+    Heroic_Effect,
+    PackBehavior_Effect,
+    Symbiosis_Effect,
+    Apealing_Effect,
+    Bark_Effect,
+    Branches_Effect,
+    DeepRoots_Effect,
+    Fecundity_Effect,
+    Fortunate_Effect,
+    Leaves_Effect,
+    Overgrowth_Effect,
+    Photosynthesis_Effect,
+    Pollination_Effect,
+    Propagation_Effect,
+    RandomFertilization_Effect,
+    SelfReplicating_Effect,
+    SwarmFur_Effect,
+    SwarmHorns_Effect,
+    SwarmMindless_Effect,
+    SwarmSpots_Effect,
+    SwarmStripes_Effect,
+    TinyLittleMelons_Effect,
+    Trunk_Effect,
+    WoodyStems_Effect,
+    Denial_Effect,
+    Faith_Effect,
+    OptimisticNihilism_Effect,
+    Altruistic_Effect,
+    Boredom_Effect,
+    Confusion_Effect,
+    Delicious_Effect,
+    Doting_Effect,
+    Eloquence_Effect,
+    Fear_Effect,
+    Flatulence_Effect,
+    Gratitude_Effect,
+    Introspective_Effect,
+    Just_Effect,
+    Late_Effect,
+    Mindful_Effect,
+    Mitochondrion_Effect,
+    Morality_Effect,
+    Prepper_Effect,
+    Saudade_Effect,
+    SelfAwareness_Effect,
+    TheThirdEye_Effect,
+    Camouflage_Effect,
+    Vampirism_Effect,
+    Viral_Effect,
+    Adorable_Effect,
+    BigEars_Effect,
+    Clever_Effect,
+    DirectlyRegister_Effect,
+    Dreamer_Effect,
+    FineMotorSkills_Effect,
+    Impatience_Effect,
+    Inventive_Effect,
+    Memory_Effect,
+    Nocturnal_Effect,
+    Nosy_Effect,
+    Parasitic_Effect,
+    Persuasive_Effect,
+    Poisonous_Effect,
+    Selfish_Effect,
+    Sneaky_Effect,
+    StickySecretions_Effect,
+    SuperSpreader_Effect,
+    Teeth_Effect,
+    Telekinetic_Effect,
+    Venomous_Effect,
+    ApexPredator_Effect,
+    HyperIntelligence_Effect,
+    Sentience_Effect,
+    Antlers_Effect,
+    Bad_Effect,
+    Brave_Effect,
+    BruteStrength_Effect,
+    Endurance_Effect,
+    Fangs_Effect,
+    FireSkin_Effect,
+    HeatVision_Effect,
+    HotTemper_Effect,
+    KidneyBeerHelm_Effect,
+    KidneyChefsToque_Effect,
+    KidneyCombover_Effect,
+    KidneyElfHat_Effect,
+    KidneyPartyHat_Effect,
+    KidneyTiara_Effect,
+    Quick_Effect,
+    Reckless_Effect,
+    RetractableClaws_Effect,
+    StoneSkin_Effect,
+    Territorial_Effect,
+    Voracious_Effect,
+    WarmBlood_Effect,
+};
