567d1dbfbb37c16ed87de2734db258746c5fe7b8
Gaurab <tur61383@temple.edu>
2026-03-25 11:31:18 +0900

    fixed so website can load

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,23 +9,24 @@ use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    // show the game page
+    public function index()
+    {
+        $games = Game::all();
+        return view('home', compact('games'));
+    }
+
     public function show(Game $game)
     {
         $game->load(['players.user', 'messages.user']);
-
         return view('game.show', ['game' => $game]);
     }
 
-    // handle a player's turn
     public function playTurn(Request $request, Game $game)
     {
-        // make sure it's this player's turn
         if ($game->current_turn !== auth()->id()) {
             return response()->json(['error' => 'Not your turn'], 403);
         }
 
-        // save the updated game state
         $game->update([
             'current_turn'      => $request->current_turn,
             'catastrophe_count' => $request->catastrophe_count,
@@ -34,7 +35,6 @@ class GameController extends Controller
             'status'            => $request->status ?? $game->status,
         ]);
 
-        // save each player's updated hand
         foreach ($request->players as $playerData) {
             GamePlayer::where('game_id', $game->id)
                       ->where('user_id', $playerData['id'])
@@ -45,24 +45,8 @@ class GameController extends Controller
                       ]);
         }
 
-        // broadcast to all other players
         broadcast(new TurnPlayed($game))->toOthers();
 
         return response()->json(['status' => 'ok']);
     }
-}<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-use App\Models\Game;
-
-class GameController extends Controller
-{
-    public function index()
-{
-    $games = Game::all();
-    return view('home', compact('games'));
-}
 }
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LobbyController;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\ChatController;
-use Illuminate\Support\Facades\DB;
 
 Route::get('/', [GameController::class, 'index'])->name('home');
+
+Route::get('/game/{game}', [GameController::class, 'show'])->name('game.show');
+Route::post('/game/{game}/turn', [GameController::class, 'playTurn'])->name('game.turn');
 
 Route::get('/doomling', function () {
     return view('doomling');
 })->name('doomling');
-
-Route::get('/Authentification', function () {
+Route::get('/login', function () {
     return view('Authentification');
-})->name('Authentification');
-
+})->name('authentication');
