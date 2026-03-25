7fbe6aabff3a6dcbc393ff8f8a246d85b942941f
Amo-Kash <des.baugh@yahoo.com>
2026-03-25 15:00:03 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,31 @@ Route::get('/doomling', function () {
 })->name('doomling');
 Route::get('/login', function () {
     return view('Authentification');
+<<<<<<< HEAD
 })->name('authentication');
+=======
+})->name('Authentification');
+
+<<<<<<< HEAD
+=======
+    // game
+    Route::get('/game/{game}', [GameController::class, 'show']);
+    Route::post('/game/{game}/turn', [GameController::class, 'playTurn']);
+
+    // chat
+    Route::post('/game/{game}/chat', [ChatController::class, 'send']);
+<<<<<<< Updated upstream
+    Route::get('/cards', function () {
+        return DB::table('doomlings_deck')->get();
+    });
+});
+=======
+    
+});
+
+Route::get('/cards', function(){
+    return DB::table('doomlings_deck')->get();
+});
+>>>>>>> Stashed changes
+>>>>>>> eb33814 (temp)
+>>>>>>> 3d037f9 (temp)
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [
         laravel({
+<<<<<<< Updated upstream
             input: ["resources/css/app.css", "resources/js/app.jsx"],
+=======
+            input: ["resources/css/app.css", "resources/js/app.js"],
+>>>>>>> Stashed changes
             refresh: true,
         }),
         tailwindcss(),
