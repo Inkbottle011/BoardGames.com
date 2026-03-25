835053d5c544af1232a2e6ceafb593a60d298ba7
Amo-Kash <des.baugh@yahoo.com>
2026-03-25 21:01:55 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,42 +4,29 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LobbyController;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\ChatController;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', [GameController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
-Route::get('/game/{game}', [GameController::class, 'show'])->name('game.show');
-Route::post('/game/{game}/turn', [GameController::class, 'playTurn'])->name('game.turn');
+Route::view('/doomling', 'doomling')->name('doomling');
 
-Route::get('/doomling', function () {
-    return view('doomling');
-})->name('doomling');
-Route::get('/login', function () {
-    return view('Authentification');
-<<<<<<< HEAD
-})->name('authentication');
-=======
-})->name('Authentification');
+Route::middleware(['auth'])->group(function () {
+    // lobby
+    Route::get('/lobby', [LobbyController::class, 'index']);
+    Route::post('/lobby/create', [LobbyController::class, 'create']);
+    Route::post('/lobby/join/{game}', [LobbyController::class, 'join']);
 
-<<<<<<< HEAD
-=======
     // game
     Route::get('/game/{game}', [GameController::class, 'show']);
     Route::post('/game/{game}/turn', [GameController::class, 'playTurn']);
 
     // chat
     Route::post('/game/{game}/chat', [ChatController::class, 'send']);
-<<<<<<< Updated upstream
-    Route::get('/cards', function () {
-        return DB::table('doomlings_deck')->get();
-    });
-});
-=======
     
 });
 
 Route::get('/cards', function(){
     return DB::table('doomlings_deck')->get();
 });
->>>>>>> Stashed changes
->>>>>>> eb33814 (temp)
->>>>>>> 3d037f9 (temp)
