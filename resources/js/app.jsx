9be562fb365a32461af95a7cfc8966d9dbac83a0
Gaurab <tur61383@temple.edu>
2026-03-25 10:20:30 +0900

    A

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,2 @@
 import "./bootstrap";
-<<<<<<< HEAD
 import "./echo";
-=======
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Doom from "./doom";
-
-ReactDOM.createRoot(document.getElementById("app")).render(<Doom />);
->>>>>>> 8fca6fae11bd2028878c81ee6c40bce8ff6dcd23
